cee1a01ce82b4a5eb5d4d1a3df2a478a9277a9b9
Joel Lee <lee.joel395@gmail.com>
2023-10-20 21:29:09 -0700

    player health/timer + basic hazard wip

---
+++ Assets/HealthHazard.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthHazard : MonoBehaviour
+{
+    float damageValue = 5;
+}
---
+++ Assets/PlayerHealthTimer.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHealthTime = 100.0f; //time in seconds?
+    [SerializeField]
+    private float timeSpeed = 1; //how fast does the timer move down?
+
+    [SerializeField]
+    private Image timeBarSprite = null;
+
+    private float curHealthTime;
+    [SerializeField]
+    bool isLevelStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealthTime = maxHealthTime;   
+    }
+
+    private void Update()
+    {
+        if (isLevelStarted)
+        {
+            curHealthTime -= timeSpeed * Time.deltaTime;
+            UpdateUI();
+            if (curHealthTime <= 0)
+            {
+                curHealthTime = 0;
+                isLevelStarted = false;
+                GameOver();
+            }
+        }    
+    }
+
+    /// <summary>
+    /// modify the speed at which health drains
+    /// </summary>
+    /// <param name="newModifier"></param>
+    public void ModifyHealthDrain(float newModifier)
+    {
+        timeSpeed = newModifier;
+    }
+
+    /// <summary>
+    /// remove a chunk of health
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeHealthDamage(float damage)
+    {
+        curHealthTime -= damage;
+        UpdateUI();
+    }
+
+    /// <summary>
+    /// Updates healthbar and anything else related to health
+    /// </summary>
+    private void UpdateUI()
+    {
+        timeBarSprite.fillAmount = curHealthTime / maxHealthTime;
+    }
+
+    /// <summary>
+    /// handle game over stuff
+    /// </summary>
+    private void GameOver()
+    {
+        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
